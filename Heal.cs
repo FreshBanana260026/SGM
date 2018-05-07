96338154f3bd325188fec844e01ac7196f918df9
Patrik <260026@via.dk>
2018-05-07 23:44:50 +0200

    initial

---
+++ Heal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour {
+
+	void Start () {
+        Destroy(gameObject, 15f);
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().HealNinja();
+            Destroy(gameObject);
+        }
+        else if(collision.gameObject.tag == "Player2")
+        {
+            collision.gameObject.GetComponent<Player>().HealNinja();
+            Destroy(gameObject);
+        }
+            
+    }
+}
